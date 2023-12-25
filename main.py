b99aa1464c27ad266665f3514b68241441f430b0
Hbear10 <140596511+Hbear10@users.noreply.github.com>
2023-12-25 17:16:49 +0000

    remove unecessary backslashes in main.py
    ^^^^^^

--- main.py
+++ main.py
@@ -3,48 +3,45 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
 from ctypes import windll
+
 windll.shcore.SetProcessDpiAwareness(1)
 
 pokemons = []
 
-bulbasaur = Pokemon("Bulbasaur",1,1,"Grass", "Poison", "Seed Pokémon", 0.7, 6.9, \
-                    ["Overgrow"],["Chlorophyll"],45,49,49,65,65,45)
+bulbasaur = Pokemon("Bulbasaur", 1, 1, "Grass", "Poison", "Seed Pokémon", 0.7, 6.9,
+                    ["Overgrow"], ["Chlorophyll"], 45, 49, 49, 65, 65, 45)
 bulbasaur.add_to_list(pokemons)
 
-shuckle=Pokemon("Shuckle",213,2,"Bug","Rock","Mold Pokémon",0.6,20.5, ["Sturdy","Gluttony"],\
-                ["Contrary"], 20,10,230,10,230,5)
+shuckle = Pokemon("Shuckle", 213, 2, "Bug", "Rock", "Mold Pokémon", 0.6, 20.5, ["Sturdy", "Gluttony"],
+                  ["Contrary"], 20, 10, 230, 10, 230, 5)
 shuckle.add_to_list(pokemons)
 
-crabominable = Pokemon("Crabominable",740,7,"Fighting", "Ice", "Woolly Crab Pokémon", 1.7, 180.0, \
-                    ["Hyper Cuttter","Iron Fist"],["Anger Point"],97,132,77,62,67,43)
+crabominable = Pokemon("Crabominable", 740, 7, "Fighting", "Ice", "Woolly Crab Pokémon", 1.7, 180.0,
+                       ["Hyper Cuttter", "Iron Fist"], ["Anger Point"], 97, 132, 77, 62, 67, 43)
 crabominable.add_to_list(pokemons)
 
-pawmot = Pokemon("Pawmot",923,9,"Electric","Fighting","Hands-On Pokémon",0.9,41.0, \
-                ["Volt Absorb", "Natural Cure"],["Iron Fist"],70,115,70,70,60,105)
+pawmot = Pokemon("Pawmot", 923, 9, "Electric", "Fighting", "Hands-On Pokémon", 0.9, 41.0,
+                 ["Volt Absorb", "Natural Cure"], ["Iron Fist"], 70, 115, 70, 70, 60, 105)
 pawmot.add_to_list(pokemons)
-
-
 
 print(pokemons)
 
+font = "Joystix Monospace"
 
+count = 1
 
-font="Joystix Monospace"
-
-count=1
 
 def addonecount():
     global count
 
-    if count == len(pokemons)-1:
-        count=0
+    if count == len(pokemons) - 1:
+        count = 0
     else:
-        count +=1
+        count += 1
 
     pokemon_image = tk.PhotoImage(file=f"./assets/{pokemons[count].name.lower()}.png")
     pokemon_image_label = ttk.Label(root, image=pokemon_image, background="red", borderwidth=0)
     pokemon_image_label.place(relx=0.7, rely=0)
-
 
     name.destroy()
     type1.destroy()
@@ -62,6 +59,7 @@ def addonecount():
     specialDefence.destroy()
     make_things_that_you_see()
     pokemon_image_label.destroy()
+
 
 root = tk.Tk()
 
@@ -83,48 +81,46 @@ def type_colour_check(type):
         typebg = "light blue"
         typefg = "black"
     elif type == "Electric":
-        typebg="yellow"
-        typefg="black"
+        typebg = "yellow"
+        typefg = "black"
     elif type == "Bug":
         typebg = "#996600"
         typefg = "white"
     elif type == "Rock":
         typebg = "#333300"
-        typefg ="#ffffff"
+        typefg = "#ffffff"
     else:
         typebg = "black"
         typefg = "white"
 
 
 def make_things_that_you_see():
-    global name,number,type1,type2,generation,species,weight,height,hp,attack,defence,specialAttack,specialDefence,speed,total\
-        ,next,pokemon_image_label,typebg,typefg,pokemon_image
-
+    global name, number, type1, type2, generation, species, weight, height, hp, attack, defence, specialAttack, specialDefence, speed, total \
+        , next, pokemon_image_label, typebg, typefg, pokemon_image
 
     name = ttk.Label(root, text=pokemons[count].name, font=(font, 20))
 
     numberlen = len(str(pokemons[count].number))
-    if numberlen <1:
+    if numberlen < 1:
         number = (ttk.Label(root, text="#0000", font=(font, 17)))
     elif numberlen == 1:
-        number = (ttk.Label(root, text=f"#000{pokemons[count].number}",font=(font, 17)))
+        number = (ttk.Label(root, text=f"#000{pokemons[count].number}", font=(font, 17)))
     elif numberlen == 2:
-        number = (ttk.Label(root, text=f"#00{pokemons[count].number}",font=(font, 17)))
+        number = (ttk.Label(root, text=f"#00{pokemons[count].number}", font=(font, 17)))
     elif numberlen == 3:
-        number = (ttk.Label(root, text=f"#0{pokemons[count].number}",font=(font, 17)))
+        number = (ttk.Label(root, text=f"#0{pokemons[count].number}", font=(font, 17)))
     elif numberlen == 4:
-        number = (ttk.Label(root, text=f"#{pokemons[count].number}",font=(font, 17)))
+        number = (ttk.Label(root, text=f"#{pokemons[count].number}", font=(font, 17)))
     else:
-        number = (ttk.Label(root, text="#Error",font=(font, 17)))
+        number = (ttk.Label(root, text="#Error", font=(font, 17)))
 
-    typebg="white"
-    typefg="black"
-
+    typebg = "white"
+    typefg = "black"
 
     type_colour_check(pokemons[count].type1)
-    type1 = (ttk.Label(root, text=pokemons[count].type1,font=(font, 17),background=typebg,foreground=typefg))
+    type1 = (ttk.Label(root, text=pokemons[count].type1, font=(font, 17), background=typebg, foreground=typefg))
     type_colour_check(pokemons[count].type2)
-    type2 = (ttk.Label(root, text=pokemons[count].type2,font=(font, 17),background=typebg,foreground=typefg))
+    type2 = (ttk.Label(root, text=pokemons[count].type2, font=(font, 17), background=typebg, foreground=typefg))
     generation = (ttk.Label(root, text=f"Gen: {pokemons[count].generation}", font=(font, 17)))
     species = (ttk.Label(root, text=pokemons[count].species, font=(font, 14)))
     height = (ttk.Label(root, text=f"Height: {pokemons[count].height}", font=(font, 12)))
@@ -139,12 +135,10 @@ def make_things_that_you_see():
     speed = (ttk.Label(root, text=f"Speed: {pokemons[count].speed}", font=(font, 12)))
     total = (ttk.Label(root, text=f"Total: {pokemons[count].total}", font=(font, 12)))
 
-
     pokemon_image = ImageTk.PhotoImage(Image.open(f"./assets/{pokemons[count].name.lower()}.png"))
-    pokemon_image_label = ttk.Label(root, image=pokemon_image, background="red",borderwidth=0)
+    pokemon_image_label = ttk.Label(root, image=pokemon_image, background="red", borderwidth=0)
 
-    next=tk.Button(root, font=(font,12),bg="yellow",fg="black",text=">", command=addonecount)
-
+    next = tk.Button(root, font=(font, 12), bg="yellow", fg="black", text=">", command=addonecount)
 
     name.place(relx=0.025, rely=0.05)
     number.place(relx=0.025, rely=0.139)
@@ -166,20 +160,14 @@ def make_things_that_you_see():
 
     next.place(relx=0.9, rely=0.9)
 
-
     pokemon_image_label.place(relx=0.7, rely=0)
 
 
-
 make_things_that_you_see()
-
-
-
 
 root.config(bg="red")
 root.title("Pythodex")
 root.geometry("550x600+50+50")
 root.iconbitmap("./assets/PythodexLogo.ico")
-
 
 root.mainloop()
