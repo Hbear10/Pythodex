38dccc7536f87890171930cc24563a78c59d7d15
Hbear10 <140596511+Hbear10@users.noreply.github.com>
2023-12-27 06:56:34 +0000

    reformatting main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Pokemon import Pokemon
 import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
-import sys
+
 from ctypes import windll
 
 windll.shcore.SetProcessDpiAwareness(1)
@@ -39,23 +39,17 @@ crabominable.add_to_list(pokemons)
 
 wyrdeer = Pokemon("", 1.8, 95.1,
                   ["Intimidate", "Frisk"], ["Sad Sipper"], 103, 105, 72, 105, 75, 65)
-wyrdeer.add_to_list(pokemons)
-
 pawmot = Pokemon("", 0.9, 41.0,
                  ["Volt Absorb", "Natural Cure"], ["Iron Fist"], 70, 115, 70, 70, 60, 105)
-pawmot.add_to_list(pokemons)
-
-gholdengo = Pokemon("Gholdengo", 1000, 9, "Steel", "Ghost", "Coin Entity Pokémon", 1.2, 30.0, ["Good as gold"], [], 87,
+gholdengo =1.2, 30.0, ["Good as gold"], [], 87,
                     60, 95, 133, 91, 84)
-gholdengo.add_to_list(pokemons)
-
-print(pokemons)
 '''
 font = "Joystix Monospace"
 
 count = 0
 
 
+#file handling
 def info_get_file():
     global pokemons
 
@@ -65,7 +59,7 @@ def info_get_file():
         file_length = len(lines)
 
         legendaries = lines[0].split(",")
-        legendaries[-1] = (legendaries[-1]).replace("\n","")
+        legendaries[-1] = (legendaries[-1]).replace("\n", "")
 
         if len(legendaries) == 0:
             pass
@@ -77,7 +71,7 @@ def info_get_file():
                     pass
 
         mythicals = lines[1].split(",")
-        mythicals[-1] = (mythicals[-1]).replace("\n","")
+        mythicals[-1] = (mythicals[-1]).replace("\n", "")
 
         if len(mythicals) == 0:
             pass
@@ -92,14 +86,101 @@ def info_get_file():
             print("No data")
         else:
             listofpoke = {}
-            for i in range(2,file_length):
+            for i in range(2, file_length):
                 line = (lines[i]).split(",")
                 pokename = line[0]
-                listofpoke[pokename.lower()] = Pokemon(line[0],line[1],line[2],line[3],line[4],line[5].replace("\n",""))
+                listofpoke[pokename.lower()] = Pokemon(line[0], line[1], line[2], line[3], line[4],
+                                                       line[5].replace("\n", ""))
 
             for pokename, obj in listofpoke.items():
                 globals()[pokename] = obj
                 eval(pokename).add_to_list(pokemons)
+
+
+#main pokemon menu
+def main_pokemon():
+    global name, number, type1, type2, generation, species, pokemon_image_label, next, \
+        typebg, typefg, pokemon_image, count, icon_image, icon
+    # weight, height,hp, attack, defence, specialAttack, specialDefence, speed, total
+
+    name = ttk.Label(root, text=pokemons[count].name, font=(font, 25), foreground="#0f0f0a")
+
+    numberlen = len(str(pokemons[count].number))
+    if numberlen < 1:
+        number = (ttk.Label(root, text="#0000", font=(font, 17), foreground="#6b6b47"))
+    elif numberlen == 1:
+        number = (ttk.Label(root, text=f"#000{pokemons[count].number}", font=(font, 17), foreground="#6b6b47"))
+    elif numberlen == 2:
+        number = (ttk.Label(root, text=f"#00{pokemons[count].number}", font=(font, 17), foreground="#6b6b47"))
+    elif numberlen == 3:
+        number = (ttk.Label(root, text=f"#0{pokemons[count].number}", font=(font, 17), foreground="#6b6b47"))
+    elif numberlen == 4:
+        number = (ttk.Label(root, text=f"#{pokemons[count].number}", font=(font, 17), foreground="#6b6b47"))
+    else:
+        number = (ttk.Label(root, text="#Error", font=(font, 17)))
+
+    typebg = "white"
+    typefg = "black"
+
+    type_colour_check(pokemons[count].type1)
+    type1 = (ttk.Label(root, text=pokemons[count].type1, font=(font, 17), background=typebg, foreground=typefg))
+    type_colour_check(pokemons[count].type2)
+    type2 = (ttk.Label(root, text=pokemons[count].type2, font=(font, 17), background=typebg, foreground=typefg))
+    generation = (ttk.Label(root, text=f"Gen: {pokemons[count].generation}", font=(font, 12), foreground="#6b6b47"))
+    species = (ttk.Label(root, text=pokemons[count].species, font=(font, 17), foreground="#6b6b47"))
+    # height = (ttk.Label(root, text=f"Height: {pokemons[count].height}m", font=(font, 12),foreground="#6b6b47"))
+    # weight = (ttk.Label(root, text=f"Weight: {pokemons[count].weight}kg", font=(font, 12),foreground="#6b6b47"))
+    # ability = (ttk.Label(root, text=pokemons[count].abilities, font=(font, 12),foreground="#6b6b47"))
+    # hiddenAbility = (ttk.Label(root, text=pokemons[count].habilities, font=(font, 12),foreground="#6b6b47"))
+    # hp = (ttk.Label(root, text=f"HP: {pokemons[count].hp}", font=(font, 12),foreground="#6b6b47"))
+    # attack = (ttk.Label(root, text=f"Attack: {pokemons[count].attack}", font=(font, 12),foreground="#6b6b47"))
+    # defence = (ttk.Label(root, text=f"Defence: {pokemons[count].defence}", font=(font, 12),foreground="#6b6b47"))
+    # specialAttack = (ttk.Label(root, text=f"Special Attack: {pokemons[count].spattack}", font=(font, 12),foreground="#6b6b47"))
+    # specialDefence = (ttk.Label(root, text=f"Special Defence: {pokemons[count].spdefence}", font=(font, 12),foreground="#6b6b47"))
+    # speed = (ttk.Label(root, text=f"Speed: {pokemons[count].speed}", font=(font, 12),foreground="#6b6b47"))
+    # total = (ttk.Label(root, text=f"Total: {pokemons[count].total}", font=(font, 12),foreground="#6b6b47"))
+
+    pokemon_image = ImageTk.PhotoImage(Image.open(f"./assets/{pokemons[count].name.lower()}.png"))
+    pokemon_image_label = ttk.Label(root, image=pokemon_image, background="red", borderwidth=0)
+
+    next = tk.Button(root, font=(font, 12), bg="yellow", fg="black", text=">",
+                     command=lambda: [destroy_main_pokemon_menu(), addonecount()])
+    last = tk.Button(root, font=(font, 12), bg="yellow", fg="black", text="<",
+                     command=lambda: [destroy_main_pokemon_menu(), minusonecount()])
+
+    if pokemons[count].legendary:
+        icon_image = ImageTk.PhotoImage(Image.open(f"./assets/legendary_Preview.png"))
+    else:
+        icon_image = ImageTk.PhotoImage(Image.open(f"./assets/myth.png"))
+    icon = ttk.Label(root, image=icon_image, background="red", borderwidth=0)
+
+    if pokemons[count].legendary or pokemons[count].mythical:
+        icon.place(relx=0.7, rely=0.05)
+    else:
+        icon.place(relx=1, rely=1)
+
+    name.place(relx=0.025, rely=0.05)
+    number.place(relx=0.025, rely=0.3)
+    type1.place(relx=0.025, rely=0.225)
+    type2.place(relx=0.4, rely=0.225)
+    generation.place(relx=0.025, rely=0.9)
+    species.place(relx=0.025, rely=0.15)
+
+    next.place(relx=0.9, rely=0.9)
+    last.place(relx=0.8, rely=0.9)
+
+    pokemon_image_label.place(relx=0.25, rely=0.4)
+
+    # height.place(relx=0.025, rely=0.4)
+    # weight.place(relx=0.5, rely=0.4)
+
+    # hp.place(relx=0.025, rely=0.475)
+    # attack.place(relx=0.025, rely=0.53)
+    # defence.place(relx=0.025, rely=0.595)
+    # specialAttack.place(relx=0.025, rely=0.66)
+    # specialDefence.place(relx=0.025, rely=0.725)
+    # speed.place(relx=0.025, rely=0.785)
+    # total.place(relx=0.025, rely=0.845)
 
 
 def destroy_main_pokemon_menu():
@@ -207,6 +288,7 @@ def type_colour_check(type):
         typefg = "white"
 
 
+# start screen
 def start_screen_destroy():
     start_screen.destroy()
     start_button.destroy()
@@ -224,93 +306,9 @@ def start_screen():
     start_button.place(relx=0.4, rely=0.85)
 
 
-def main_pokemon():
-    global name, number, type1, type2, generation, species, pokemon_image_label, next, \
-        typebg, typefg, pokemon_image, count, icon_image, icon
-    # weight, height,hp, attack, defence, specialAttack, specialDefence, speed, total
-
-    name = ttk.Label(root, text=pokemons[count].name, font=(font, 25), foreground="#0f0f0a")
-
-    numberlen = len(str(pokemons[count].number))
-    if numberlen < 1:
-        number = (ttk.Label(root, text="#0000", font=(font, 17), foreground="#6b6b47"))
-    elif numberlen == 1:
-        number = (ttk.Label(root, text=f"#000{pokemons[count].number}", font=(font, 17), foreground="#6b6b47"))
-    elif numberlen == 2:
-        number = (ttk.Label(root, text=f"#00{pokemons[count].number}", font=(font, 17), foreground="#6b6b47"))
-    elif numberlen == 3:
-        number = (ttk.Label(root, text=f"#0{pokemons[count].number}", font=(font, 17), foreground="#6b6b47"))
-    elif numberlen == 4:
-        number = (ttk.Label(root, text=f"#{pokemons[count].number}", font=(font, 17), foreground="#6b6b47"))
-    else:
-        number = (ttk.Label(root, text="#Error", font=(font, 17)))
-
-    typebg = "white"
-    typefg = "black"
-
-    type_colour_check(pokemons[count].type1)
-    type1 = (ttk.Label(root, text=pokemons[count].type1, font=(font, 17), background=typebg, foreground=typefg))
-    type_colour_check(pokemons[count].type2)
-    type2 = (ttk.Label(root, text=pokemons[count].type2, font=(font, 17), background=typebg, foreground=typefg))
-    generation = (ttk.Label(root, text=f"Gen: {pokemons[count].generation}", font=(font, 12), foreground="#6b6b47"))
-    species = (ttk.Label(root, text=pokemons[count].species, font=(font, 17), foreground="#6b6b47"))
-    # height = (ttk.Label(root, text=f"Height: {pokemons[count].height}m", font=(font, 12),foreground="#6b6b47"))
-    # weight = (ttk.Label(root, text=f"Weight: {pokemons[count].weight}kg", font=(font, 12),foreground="#6b6b47"))
-    # ability = (ttk.Label(root, text=pokemons[count].abilities, font=(font, 12),foreground="#6b6b47"))
-    # hiddenAbility = (ttk.Label(root, text=pokemons[count].habilities, font=(font, 12),foreground="#6b6b47"))
-    # hp = (ttk.Label(root, text=f"HP: {pokemons[count].hp}", font=(font, 12),foreground="#6b6b47"))
-    # attack = (ttk.Label(root, text=f"Attack: {pokemons[count].attack}", font=(font, 12),foreground="#6b6b47"))
-    # defence = (ttk.Label(root, text=f"Defence: {pokemons[count].defence}", font=(font, 12),foreground="#6b6b47"))
-    # specialAttack = (ttk.Label(root, text=f"Special Attack: {pokemons[count].spattack}", font=(font, 12),foreground="#6b6b47"))
-    # specialDefence = (ttk.Label(root, text=f"Special Defence: {pokemons[count].spdefence}", font=(font, 12),foreground="#6b6b47"))
-    # speed = (ttk.Label(root, text=f"Speed: {pokemons[count].speed}", font=(font, 12),foreground="#6b6b47"))
-    # total = (ttk.Label(root, text=f"Total: {pokemons[count].total}", font=(font, 12),foreground="#6b6b47"))
-
-    pokemon_image = ImageTk.PhotoImage(Image.open(f"./assets/{pokemons[count].name.lower()}.png"))
-    pokemon_image_label = ttk.Label(root, image=pokemon_image, background="red", borderwidth=0)
-
-    next = tk.Button(root, font=(font, 12), bg="yellow", fg="black", text=">",
-                     command=lambda: [destroy_main_pokemon_menu(), addonecount()])
-    last = tk.Button(root, font=(font, 12), bg="yellow", fg="black", text="<",
-                     command=lambda: [destroy_main_pokemon_menu(), minusonecount()])
-
-    if pokemons[count].legendary:
-        icon_image = ImageTk.PhotoImage(Image.open(f"./assets/legendary_Preview.png"))
-    else:
-        icon_image = ImageTk.PhotoImage(Image.open(f"./assets/myth.png"))
-    icon = ttk.Label(root, image=icon_image, background="red", borderwidth=0)
-
-    if pokemons[count].legendary or pokemons[count].mythical:
-        icon.place(relx=0.7, rely=0.05)
-    else:
-        icon.place(relx=1, rely=1)
-
-    name.place(relx=0.025, rely=0.05)
-    number.place(relx=0.025, rely=0.3)
-    type1.place(relx=0.025, rely=0.225)
-    type2.place(relx=0.4, rely=0.225)
-    generation.place(relx=0.025, rely=0.9)
-    species.place(relx=0.025, rely=0.15)
-
-    next.place(relx=0.9, rely=0.9)
-    last.place(relx=0.8, rely=0.9)
-
-    pokemon_image_label.place(relx=0.25, rely=0.4)
-
-    # height.place(relx=0.025, rely=0.4)
-    # weight.place(relx=0.5, rely=0.4)
-
-    # hp.place(relx=0.025, rely=0.475)
-    # attack.place(relx=0.025, rely=0.53)
-    # defence.place(relx=0.025, rely=0.595)
-    # specialAttack.place(relx=0.025, rely=0.66)
-    # specialDefence.place(relx=0.025, rely=0.725)
-    # speed.place(relx=0.025, rely=0.785)
-    # total.place(relx=0.025, rely=0.845)
-
+root = tk.Tk()
 
 info_get_file()
-root = tk.Tk()
 start_screen()
 
 root.config(bg="red")
